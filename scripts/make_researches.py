b36350e79c2bcc8dc53034e803bea4793ebb208b
Shabash4off <shambergernikita@gmail.com>
2024-02-13 07:28:01 +0700

    Add scripts for auto research making

---
+++ scripts/make_researches.py
@@ -0,0 +1,31 @@
+import math
+from utils.builder import build
+from utils.controller import test
+
+build()
+
+test_data = []
+
+# Research 1
+for i in range(1, 25):
+    x = 10**i
+    test_data.append({"pi": math.pi, "points": [(0, x), (0, 0), (x, 0)]})
+
+test('Reasearch 1', test_data)
+
+# Research 3
+test_data = []
+
+for i in range(1, 25):
+    x = 10**(-i)
+    test_data.append({"pi": math.pi, "points": [(0, x), (0, 0), (x, 0)]})
+
+test('Reasearch 2', test_data)
+
+# Research 3
+test_data = []
+
+for i in range(1, 10):
+    test_data.append({"pi": round(math.pi, i), "points": [(0, 4), (0, 0), (4, 0)]})
+
+test('Reasearch 3', test_data)
