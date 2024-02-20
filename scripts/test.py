11e2876bc0770285590f1bd39696f69749210ac5
Shabash4off <shambergernikita@gmail.com>
2024-02-20 15:03:42 +0700

    Change tests

--- scripts/test.py
+++ scripts/test.py
@@ -6,8 +6,8 @@ build()
 
 test_data = [
     {"pi": math.pi, "points": [(10, 0), (0, 0), (0, 10)]},
-    {"pi": math.pi, "points": [(320, 240), (350, 190), (380, 240)]},
-    {"pi": math.pi, "points": [(0, 0), (0, 1), (1.732, 0)]},
+    {"pi": math.pi, "points": [(10, 0), (0, 0), (5, 5 * math.sqrt(3))]},
+    {"pi": math.pi, "points": [(0, 0), (0, 1), (math.sqrt(3), 0)]},
     {"pi": math.pi, "points": [(0, 0), (0, 0), (0, 0)]},
     {"pi": math.pi, "points": [(0, 0), (0, 0), (0, 10)]},
     {"pi": math.pi, "points": [(1, 1), (2, 2), (100, 100)]},
