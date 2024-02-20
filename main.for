41afc70afd90178d565e5f34d1073afe8a66c4c3
Shabash4off <shambergernikita@gmail.com>
2024-02-20 16:31:19 +0700

    Remove some repeated code

--- main.for
+++ main.for
@@ -3,26 +3,35 @@
       DO i=1, 3
       READ *, p(i,1), p(i,2)
       END DO
+      CALL vectors
       CALL area
+      END
+
+      SUBROUTINE check_degeneracy
+      COMMON /triangle/ p(3, 2),fi,cosfi,S,pi,a_l,b_l,c_l,a(2),b(2),c(2)
+      IF (S.EQ.0.0) THEN
+      PRINT *,'ERROR: TRIANGLE IS DEGENERATE OR DOES NOT EXIST'
+      STOP
+      END IF
       END
 
       SUBROUTINE area
       COMMON /triangle/ p(3, 2),fi,cosfi,S,pi,a_l,b_l,c_l,a(2),b(2),c(2)
-      c1 = (p(2,1) - p(1,1)) * (p(3,2) - p(1,2))
-      c2 = (p(2,2) - p(1,2)) * (p(3,1) - p(1,1))
-      S = abs(c1 - c2) / 2
+      S = abs(a(1)*c(2) - a(2)*c(1)) / 2
       END
 
-      SUBROUTINE sides
+      SUBROUTINE vectors
       COMMON /triangle/ p(3, 2),fi,cosfi,S,pi,a_l,b_l,c_l,a(2),b(2),c(2)
-
       a(1) = p(2,1) - p(1,1)
       a(2) = p(2,2) - p(1,2)
       b(1) = p(3,1) - p(2,1)
       b(2) = p(3,2) - p(2,2)
       c(1) = p(1,1) - p(3,1)
       c(2) = p(1,2) - p(3,2)
+      END
 
+      SUBROUTINE sides
+      COMMON /triangle/ p(3, 2),fi,cosfi,S,pi,a_l,b_l,c_l,a(2),b(2),c(2)
       a_l=sqrt(a(1)**2+a(2)**2)
       b_l=sqrt(b(1)**2+b(2)**2)
       c_l=sqrt(c(1)**2+c(2)**2)
@@ -31,7 +40,8 @@
       SUBROUTINE m_angle
       COMMON /triangle/ p(3, 2),fi,cosfi,S,pi,a_l,b_l,c_l,a(2),b(2),c(2)
 
-      call sides
+      CALL sides
+
       IF (a_l .LE. b_l) THEN
       IF (a_l .LE. c_l) THEN
       fi = asin(2*S/(c_l*b_l))
@@ -49,8 +59,9 @@
 
       SUBROUTINE m_cos_angle
       COMMON /triangle/ p(3, 2),fi,cosfi,S,pi,a_l,b_l,c_l,a(2),b(2),c(2)
+      
+      CALL sides
 
-      call sides
       IF (a_l .LE. b_l) THEN
       IF (a_l .LE. c_l) THEN
       cosfi = (c(1)*(-b(1)) + c(2)*(-b(2))) / (c_l * b_l)
@@ -64,7 +75,6 @@
       END IF
       END
 
-
       SUBROUTINE menu
       COMMON /triangle/ p(3, 2),fi,cosfi,S,pi,a_l,b_l,c_l,a(2),b(2),c(2)
 1     PRINT *,'1) Write coordinates'
@@ -77,25 +87,16 @@
 10    CALL input
       GO TO 1
 
-20    IF (S.EQ.0.0) THEN
-      PRINT *,'ERROR: TRIANGLE IS DEGENERATE OR DOES NOT EXIST'
-      GO TO 1
-      END IF
+20    CALL check_degeneracy
       PRINT *,S
       GO TO 1
 
-30    IF (S.EQ.0.0) THEN
-      PRINT *,'ERROR: TRIANGLE IS DEGENERATE OR DOES NOT EXIST'
-      GO TO 1
-      END IF
+30    CALL check_degeneracy
       CALL m_angle
       PRINT *,fi
       GO TO 1
 
-40    IF (S.EQ.0.0) THEN
-      PRINT *,'ERROR: TRIANGLE IS DEGENERATE OR DOES NOT EXIST'
-      GO TO 1
-      END IF
+40    CALL check_degeneracy
       CALL m_cos_angle
       PRINT *,cosfi
       GO TO 1
